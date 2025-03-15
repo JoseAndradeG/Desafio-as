81f3de30003fe6a25dfd9b8100a5d480668743c8
JAndrade <andradepc.2020@gmail.com>
2025-03-15 19:42:14 -0400

    Creacion de funcion para limpiar campo de texto Nombre

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 // Declaracion de variables y  Array
+//
 let input_Amigos = document.getElementById("amigo");
 let nombre_Amigos = [];
-
+const campo = document.getElementById("amigo")
+//
 // Funcion para agregar los nombres
+//
 function agregarAmigo() {
     // Validacion de nombre vacio
     if (!input_Amigos.value){
         alert("Por favor ingresa un nombre")
     }        
     else{    
-    nombre_Amigos.push(input_Amigos.value);
-    
+     nombre_Amigos.push(input_Amigos.value);
     }
 };
 console.log(nombre_Amigos);
+function borrarCampo() {
+    if (input_Amigos.value !== ""){
+    campo.value = "";
+    campo.placeholder = "Escribe otro Nombre";
+    }
+}
+
+
