23597071e3cf66807980917ed0c312465a992842
JAndrade <andradepc.2020@gmail.com>
2025-03-15 20:50:36 -0400

    Creacion de funcion pra sortear Nombre

--- app.js
+++ app.js
@@ -2,10 +2,14 @@
 //
 let input_Amigos = document.getElementById("amigo");
 let nombre_Amigos = [];
-const campo = document.getElementById("amigo")
+const campo = document.getElementById("amigo");
+const listaNombres = document.getElementById("listaAmigos");
+let sorteado = 0;
+const amigo_Sorteado = document.getElementById("resultado");
 //
 // Funcion para agregar los nombres
 //
+
 function agregarAmigo() {
     // Validacion de nombre vacio
     if (!input_Amigos.value){
@@ -13,14 +17,21 @@ function agregarAmigo() {
     }        
     else{    
      nombre_Amigos.push(input_Amigos.value);
+     listaNombres.innerHTML += `<li>${input_Amigos.value}<li>`;
     }
 };
-console.log(nombre_Amigos);
+//console.log(nombre_Amigos);
+
+//Limpiar campo Nombre
 function borrarCampo() {
     if (input_Amigos.value !== ""){
     campo.value = "";
     campo.placeholder = "Escribe otro Nombre";
     }
 }
-
+function sortearAmigo(){
+    sorteado = (Math.floor((Math.random() * nombre_Amigos.length)));
+    const seleccionado = nombre_Amigos[sorteado];
+    amigo_Sorteado.innerHTML += `<li>${seleccionado}<li>`;
+}
 
